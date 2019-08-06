b7bd712403fe33c07c3e5685bb94336203cd531c
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-06 03:37:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,25 +2,31 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => {},
+        :age => {}
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => {},
+        :age => {}
         },
       :hero => {
-        #your key/value pairs here
+        :name => {},
+        :age => {}
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => {},
+        :age => {}
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => {},
+        :age => {}
         },
       :heroine => {
-        #your key/value pairs here
+        :name => {},
+        :age => {}
         },
       :heroine_friends => []
    }
