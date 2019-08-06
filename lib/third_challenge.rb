5909d05b9215d109af0778179b2a3fbfe59bd58f
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-08-06 03:40:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,16 +2,16 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "",
-        :age => ""
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        :name => {},
-        :age => {}
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        :name => {},
-        :age => {},
+        :name => "Romeo",
+        :age => "15",
         :status => {}
         },
       :hero_friends => []
